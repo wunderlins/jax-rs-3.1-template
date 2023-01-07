dffd69ce58849e5d2b7395b03657c7195f06f1b9
Simon Wunderlin <swunderlin@gmail.com>
2023-01-07 10:35:16 +0100

    updated container config for keycloak

---
+++ docker_env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+script_filename=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+echo "$script_filename"
+
+KEYCLOAK_VERSION=20.0.1
+KEYCLOAK_LISTEN_PORT=8444
+KEYCLOAK_DATA_DIR=$script_filename/docker-data/import
